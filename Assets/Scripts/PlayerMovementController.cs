6a3060ff122583cae51e9ab53b98ed9693857c15
juanmapf97 <juanmapf97@gmail.com>
2017-10-25 15:31:33 -0500

    First commit

---
+++ Assets/Scripts/PlayerMovementController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class PlayerMovementController : MonoBehaviour {
+
+	public Text speed;
+	private int kph;
+	private Vector3 previous;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		this.gameObject.transform.Translate (Vector3.forward * Input.GetAxis ("Mouse ScrollWheel"));
+	}
+
+	void FixedUpdate () {
+		kph = (int)(((this.transform.position - previous).magnitude / Time.deltaTime)*3.6);
+		previous = this.transform.position;
+		speed.text = kph + " km/h";
+	}
+}
---
+++ Assets/Scripts/tpController.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class tpController : MonoBehaviour {
+
+	public GameObject Obstacle;
+	private bool test = false;
+	// Use this for initialization
+	void Start () {
+		RepopulateObstacles ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnTriggerEnter(Collider col) {
+		Debug.Log ("Trigger Enter");
+		if (col.gameObject.tag == "Player") {
+			RepopulateObstacles ();
+			col.gameObject.transform.position = new Vector3 (272f, 3f, 199f);
+		}
+	}
+
+	void RepopulateObstacles ()
+	{
+		DestroyClones ();
+		for (int i = 0; i < 50; i++) {
+			var clone = Instantiate (Obstacle, new Vector3 (Random.Range (250, 290), 2.5f, Random.Range (-390f, 900f)), new Quaternion(0, 0, 0, 0));
+			clone.tag = "Clone";
+		}
+	}
+
+	void DestroyClones() {
+		var clones = GameObject.FindGameObjectsWithTag ("Clone");
+		foreach (var clone in clones){
+			Destroy(clone);
+		}
+	}
+}
